c2097e3bf790332315ea8b00c55d5b8b03c80acd
Jan van Brügge <jan.van.brugge@futurice.com>
2021-07-06 13:59:04 +0200

    nvim: Add keybindings for lsp actions

--- roles/nvim.nix
+++ roles/nvim.nix
@@ -84,8 +84,36 @@
         config =
           ''
           lua <<EOF
-          require'lspconfig'.hls.setup{}
-          require'lspconfig'.tsserver.setup{}
+          local servers = { "tsserver", "hls" }
+          
+          local nvim_lsp = require('lspconfig')
+
+          -- after the language server attaches to the current buffer
+          local on_attach = function(client, bufnr)
+            local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+            local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+            -- Mappings.
+            local opts = { noremap=true, silent=true }
+            buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            buf_set_keymap('n', '<leader>h', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+            buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+          end
+
+          -- Use a loop to conveniently call 'setup' on multiple servers and
+          -- map buffer local keybindings when the language server attaches
+          for _, lsp in ipairs(servers) do
+            nvim_lsp[lsp].setup {
+              on_attach = on_attach,
+              flags = {
+                debounce_text_changes = 150,
+              }
+            }
+          end
           EOF
           '';
       }
@@ -115,8 +143,48 @@
               nvim_lua = true;
               vsnip = true;
               ultisnips = true;
+              emoji = true;
             };
           }
+
+          local t = function(str)
+            return vim.api.nvim_replace_termcodes(str, true, true, true)
+          end
+
+          local check_back_space = function()
+              local col = vim.fn.col('.') - 1
+              return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+          end
+
+          -- Use (s-)tab to:
+          --- move to prev/next item in completion menuone
+          --- jump to prev/next snippet's placeholder
+          _G.tab_complete = function()
+            if vim.fn.pumvisible() == 1 then
+              return t "<C-n>"
+            elseif vim.fn['vsnip#available'](1) == 1 then
+              return t "<Plug>(vsnip-expand-or-jump)"
+            elseif check_back_space() then
+              return t "<Tab>"
+            else
+              return vim.fn['compe#complete']()
+            end
+          end
+          _G.s_tab_complete = function()
+            if vim.fn.pumvisible() == 1 then
+              return t "<C-p>"
+            elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+              return t "<Plug>(vsnip-jump-prev)"
+            else
+              -- If <S-Tab> is not working in your terminal, change it to <C-h>
+              return t "<S-Tab>"
+            end
+          end
+
+          vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+          vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
           EOF
           '';
       }
