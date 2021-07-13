f848899bcae966928f2104f46a6d799c2ed37c25
Jan van Brügge <jan.van.brugge@futurice.com>
2021-07-13 11:53:44 +0200

    firefox: Disable webRTC indicator

--- roles/firefox.nix
+++ roles/firefox.nix
@@ -4,5 +4,18 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-wayland;
+    profiles.jan = {
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+      userChrome =
+        ''
+        @namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"); /* set default namespace to XUL */
+
+        #webrtcIndicator {
+          display: none !important;
+        }
+        '';
+    };
   };
 }
