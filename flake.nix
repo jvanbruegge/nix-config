0f838419f5df5dc27514635fee68afbde8efcda7
Jan van Brügge <jan.van.brugge@futurice.com>
2021-09-14 13:33:31 +0200

    nix: Update nixpkgs, switch back to upstream home-manager

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:jvanbruegge/home-manager/named-mailboxes";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
