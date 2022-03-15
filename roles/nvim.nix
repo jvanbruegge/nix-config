322fa88dd2cab7df9b3d64dafaac73cb5d20730f
Jan van Brügge <jan.van.brugge@futurice.com>
2022-03-15 12:48:51 +0100

    nvim: Add shellcheck diagnostics with null-ls

--- roles/nvim.nix
+++ roles/nvim.nix
@@ -227,6 +227,19 @@
           let g:prettier#autoformat_require_pragma = 0
           '';
       }
+      { plugin = null-ls-nvim;
+        config = ''
+          lua <<EOF
+          local null_ls = require("null-ls")
+          null_ls.setup({
+            sources = {
+              null_ls.builtins.diagnostics.shellcheck,
+              null_ls.builtins.code_actions.shellcheck
+            }
+          })
+          EOF
+        '';
+      }
     ];
   };
 }
