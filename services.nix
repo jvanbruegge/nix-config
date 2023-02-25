52997d484d9001a12139b82c8bd21761d483204c
Jan van Brügge <supermanitu@gmail.com>
2023-02-25 14:23:44 +0000

    services: Add virtualization

--- services.nix
+++ services.nix
@@ -3,6 +3,16 @@
   services.fwupd.enable = true;
   services.printing.enable = true;
 
+  virtualisation = {
+    docker.enable = true;
+    virtualbox.host = {
+      enable = true;
+      headless = true;
+      enableExtensionPack = true;
+    };
+  };
+  users.users.jan.extraGroups = [ "docker" "vboxusers" ];
+
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
