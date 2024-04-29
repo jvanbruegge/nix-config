2591360b95ab8429a20a6da2cd1886c0467b704d
Jan van Brügge <supermanitu@gmail.com>
2024-04-29 14:03:42 +0100

    hardware: Remove scanner support
    The scanner is connected to the NAS, so all scanning happens through
    that

--- hardware.nix
+++ hardware.nix
@@ -4,28 +4,6 @@
   # Audio
   environment.systemPackages = [ pkgs.pavucontrol ];
 
-  # Scanners
-  hardware.sane = {
-    enable = true;
-    extraBackends = [ pkgs.epkowa ];
-    disabledDefaultBackends = [
-      "abaton" "agfafocus" "apple" "artec"
-      "as6e" "avision" "bh" "canon"
-      "cardscan" "coolscan"
-      "dell1600n_net" "dmc" "epjitsu" "epson2" "epsonds"
-      "escl" "fujitsu" "genesys" "gt68xx" "hp"
-      "hs2p" "ibm" "kodak"
-      "kvs1025" "kvs20xx" "kvs40xx" "leo"
-      "lexmark" "ma1509" "magicolor" "matsushita" "microtek"
-      "mustek" "nec"
-      "niash" "pie" "pint" "pixma" "plustek"
-      "qcam" "ricoh" "rts8891" "s9036"
-      "sceptre" "sharp" "sm3600" "sm3840" "snapscan"
-      "sp15c" "tamarack" "teco1" "teco2" "teco3"
-      "u12" "umax" "v4l" "xerox_mfp"
-    ];
-  };
-
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
