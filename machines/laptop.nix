e4fc6974479bcb9315ad591952c12be841969186
Jan van Brügge <supermanitu@gmail.com>
2025-12-08 11:55:57 +0100

    machines: Update laptop.nix

--- machines/laptop.nix
+++ machines/laptop.nix
@@ -8,32 +8,29 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd = {
-    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-    kernelModules = [ "dm-snapshot" ];
-    luks.devices.main.device = "/dev/nvme0n1p2";
-  };
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b64f0dfa-68d9-4b75-aa05-4b2814894075";
+    { device = "/dev/disk/by-uuid/acfb500d-d4fe-4419-8af8-7bbb1d594dc1";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/2caa0bbf-71ae-4ec3-960d-37bff71a9796";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4582-CE68";
+    { device = "/dev/disk/by-uuid/B272-1A11";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/8506806f-5d90-46d0-a63f-1a48297abd53";
-      fsType = "ext4";
-    };
+  swapDevices = [ ];
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/d33e0fa4-d941-4156-bcc1-d87dcd0dbdc9"; }
-    ];
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
