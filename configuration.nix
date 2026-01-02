a56d347c62b390723316d3e5b380e0a350395016
Jan van Brügge <supermanitu@gmail.com>
2026-01-02 14:20:29 +0100

    nix: Add reflex substituters

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,16 @@
       sshUser = "ubuntu";
       sshKey = "/root/.ssh/minecraft_ed25519";
     } ];
+    settings = {
+      substituters = [
+        "https://nixcache.reflex-frp.org"
+        "http://nixcache.heilmannsoftware.net"
+      ];
+      trusted-public-keys = [
+        "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+        "nixcache.heilmannsoftware.net:5kPz/A5gVCs8E96y8PlT10sdAJtAQ5n4n3E0j7UXGQA="
+      ];
+    };
   };
   nixpkgs.config.allowUnfree = true;
 
