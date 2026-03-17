9f67b0ba7b7a5e6376c74bf2477300c391a17bd0
Jan van Brügge <supermanitu@gmail.com>
2026-03-17 10:15:02 +0100

    general: Set more specific locale

--- users/jan.nix
+++ users/jan.nix
@@ -16,5 +16,19 @@ in
 
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = ["de_DE.UTF-8/UTF-8"];
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
 }
